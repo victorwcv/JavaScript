287c4e7859343c8cb1c1cad53aef2a438b53ac88
William <victorwccv@gmail.com>
2023-11-07 22:29:51 -0500

    proyecto + retos

--- proyectos-React/testimonios-fcc/src/App.js
+++ proyectos-React/testimonios-fcc/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import  Testimonio  from './componentes/Testimonio.js';
+import datos from './datos.js';
 
 function App() {
   return (
@@ -9,26 +10,26 @@ function App() {
         <h1>Esto es lo dicen nuestros alumnos</h1>
         
         <Testimonio 
-        nombre='Shawn Wang'
-        pais='Singapur'
-        imagen='shawn'
-        cargo='Ingeniero de Software'
-        empresa='Amazon'
-        testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
+        nombre= {datos.shawn.nombre}
+        pais= {datos.shawn.pais}
+        imagen= {datos.shawn.imagen}
+        cargo= {datos.shawn.cargo}
+        empresa= {datos.shawn.empresa}
+        testimonio= {datos.shawn.testimonio} />
         <Testimonio
-        nombre='Emma Bostian'
-        pais='Suecia'
-        imagen='emma'
-        cargo='Ingeniera de Software'
-        empresa='Spotify'
-        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' />
+        nombre= {datos.emma.nombre}
+        pais= {datos.emma.pais}
+        imagen= {datos.emma.imagen}
+        cargo= {datos.emma.cargo}
+        empresa= {datos.emma.empresa}
+        testimonio= {datos.emma.testimonio}  />
         <Testimonio 
-        nombre='Sarah Chima'
-        pais='Nigeria'
-        imagen='sarah'
-        cargo='Ingeniera de Software'
-        empresa='ChatDesk'
-        testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' />
+        nombre= {datos.sarah.nombre}
+        pais= {datos.sarah.pais}
+        imagen= {datos.sarah.imagen}
+        cargo= {datos.sarah.cargo}
+        empresa= {datos.sarah.empresa}
+        testimonio= {datos.sarah.testimonio} />
       </div>
     </div>
   );
--- proyectos-React/testimonios-fcc/src/componentes/Testimonio.js
+++ proyectos-React/testimonios-fcc/src/componentes/Testimonio.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
 function Testimonio(props) {
+	
 	return (
 	  <div className='contenedor-testimonio'>
 			<img
 			className='imagen-testimonio'
 			src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-			alt='foto de persona'
+			alt={`foto de ${props.nombre}`}
 			/>
 			<div className='contenedor-texto-testimonio'>
 				<p className='nombre-testimonio'>
