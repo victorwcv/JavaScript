30e76da4f6c0ab14077446a388c1d002b281c161
William <victorwccv@gmail.com>
2024-03-16 23:58:09 -0500

    comment added

--- HackerRank/Ejercicio-5.js
+++ HackerRank/Ejercicio-5.js
@@ -1,11 +1,16 @@
 // There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 
+// function to calculate paired socks from array of sock colors
 function sockMerchant(ar) {
+  // reduce array to an object with sock counts
   const mapSocks = ar.reduce((acc, curr) => {
-      acc[curr] = (acc[curr] || 0) + 1;
-      return acc;
+    // set count for current sock color
+    acc[curr] = (acc[curr] || 0) + 1;
+    // return object
+    return acc;
   }, {});
-  
+ 
+  // calculate number of paired socks
   return Object.values(mapSocks).reduce((pairs, count) => pairs + Math.floor(count / 2), 0);
 }
